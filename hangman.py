eb374c646deada6dfacff06c9b0bdffa6c3aa017
Noe Jacob <66005579+Noe-Jacob@users.noreply.github.com>
2022-04-17 12:23:46 +1000

    Update hangman to include frontend works

--- hangman.py
+++ hangman.py
@@ -1,7 +1,10 @@
 import random
+import time
 
 import flask
 from flask_sqlalchemy import SQLAlchemy
+import os, sys
+import webbrowser
 
 app = flask.Flask(__name__)
 
@@ -10,23 +13,40 @@ app = flask.Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hangman.db'
 db = SQLAlchemy(app)
 
+
+def base_path(path):
+    if getattr(sys, 'frozen', None):
+        basedir = sys._MEIPASS
+    else:
+        basedir = os.path.dirname(__file__)
+    return os.path.join(basedir, path)
+
+
 # Model
 
 def random_pk():
     return random.randint(1e9, 1e10)
 
-def random_word():
-    words = [line.strip() for line in open('words.txt') if len(line) > 10]
+
+def random_word(difficulty):
+    if difficulty == "EASY":
+        words = [line.strip() for line in open('words.txt') if 4 <= len(line) <= 6]
+    elif difficulty == "MEDIUM":
+        words = [line.strip() for line in open('words.txt') if 6 < len(line) <= 9]
+    else:
+        words = [line.strip() for line in open('words.txt') if 9 < len(line)]
     return random.choice(words).upper()
 
 class Game(db.Model):
     pk = db.Column(db.Integer, primary_key=True, default=random_pk)
-    word = db.Column(db.String(50), default=random_word)
+    word = db.Column(db.String(50), default=random_word("EASY"))
     tried = db.Column(db.String(50), default='')
     player = db.Column(db.String(50))
 
-    def __init__(self, player):
-        self.player = player
+    def __init__(self, player, difficulty):
+        self.player = player.capitalize()
+        self.difficulty = difficulty
+        self.word = random_word(difficulty.upper())
 
     @property
     def errors(self):
@@ -40,12 +60,31 @@ class Game(db.Model):
     def points(self):
         return 100 + 2*len(set(self.word)) + len(self.word) - 10*len(self.errors)
 
+    @property
+    def real_errors(self):
+        str1 = ""
+        for i in range(0, len(self.tried)):
+            if self.tried[i] in set(self.word):
+                str1 = self.tried[i:]
+                break
+        return ''.join(set(str1) - set(self.word))
+
     # Play
 
     def try_letter(self, letter):
-        if not self.finished and letter not in self.tried:
-            self.tried += letter
-            db.session.commit()
+        if not self.finished:
+            if len(letter) == 1 and letter not in self.tried:
+                self.tried += letter
+                db.session.commit()
+            elif letter == self.word:
+                self.tried = self.word
+                db.session.commit()
+            elif letter != self.word:
+                for i in range(65,91):
+                    if chr(i) not in set(self.word.upper()) and len(self.errors)<6:
+                        self.tried += chr(i)
+                db.session.commit()
+
 
     # Game status
 
@@ -55,7 +94,7 @@ class Game(db.Model):
 
     @property
     def lost(self):
-        return len(self.errors) == 6
+        return len(self.real_errors) >= 6
 
     @property
     def finished(self):
@@ -74,9 +113,11 @@ def home():
 @app.route('/play')
 def new_game():
     player = flask.request.args.get('player')
-    game = Game(player)
+    difficulty = flask.request.args.get('difficulty_level')
+    game = Game(player, difficulty)
     db.session.add(game)
     db.session.commit()
+    time.sleep(2)
     return flask.redirect(flask.url_for('play', game_id=game.pk))
 
 @app.route('/play/<game_id>', methods=['GET', 'POST'])
@@ -85,13 +126,14 @@ def play(game_id):
 
     if flask.request.method == 'POST':
         letter = flask.request.form['letter'].upper()
-        if len(letter) == 1 and letter.isalpha():
+        if letter.isalpha() and (len(letter) == 1 or len(letter) == len(game.word)):
             game.try_letter(letter)
 
     if flask.request.is_xhr:
         return flask.jsonify(current=game.current,
                              errors=game.errors,
-                             finished=game.finished)
+                             finished=game.finished,
+                             real_errors=game.real_errors)
     else:
         return flask.render_template('play.html', game=game)
 
@@ -99,4 +141,3 @@ def play(game_id):
 
 if __name__ == '__main__':
     app.run()
-
