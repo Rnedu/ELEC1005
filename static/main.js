d2d1b166480a80a4cfdd30955d7a31af54fcf9c9
Noe Jacob <66005579+Noe-Jacob@users.noreply.github.com>
2022-04-17 18:43:18 +1000

    Debug code

--- static/main.js
+++ static/main.js
@@ -25,6 +25,11 @@ $('#letter-form').submit(function(e) {
           'Errors (' + data.real_errors.length + '/6): ' +
 
           '<span class="text-danger spaced">' + data.real_errors + '</span>');
+
+        $('#tried').html(
+          'Tried (' + data.tried.length + '): ' +
+  
+          '<span class="text-danger spaced">' + data.tried + '</span>');
           
         /* Update drawing */
         updateDrawing(data.real_errors);
@@ -38,16 +43,3 @@ $('#letter-form').submit(function(e) {
 function updateDrawing(real_errors) {
   $('#hangman-drawing').children().slice(0, real_errors.length).show();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
