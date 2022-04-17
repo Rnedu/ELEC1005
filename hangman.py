c9eaa19a8ef3e993328b0a2d48c38244e2747abe
Noe Jacob <66005579+Noe-Jacob@users.noreply.github.com>
2022-04-17 18:40:09 +1000

    Debug the code

--- hangman.py
+++ hangman.py
@@ -8,33 +8,35 @@ import webbrowser
 
 app = flask.Flask(__name__)
 
-# Database
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hangman.db'
-db = SQLAlchemy(app)
-
+# Changing directory
 
 def base_path(path):
     if getattr(sys, 'frozen', None):
         basedir = sys._MEIPASS
     else:
-        basedir = os.path.dirname(__file__)
+        basedir =  os.path.abspath(os.path.dirname(__file__))
     return os.path.join(basedir, path)
 
+# Database 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'hangman.db')
+db = SQLAlchemy(app)
 
 # Model
 
 def random_pk():
     return random.randint(1e9, 1e10)
 
+# Selecting random word from words.txt according to the difficulty
 
 def random_word(difficulty):
     if difficulty == "EASY":
-        words = [line.strip() for line in open('words.txt') if 4 <= len(line) <= 6]
+        words = [line.strip() for line in open(base_path('words.txt')) if 4 <= len(line.strip()) <= 6]
     elif difficulty == "MEDIUM":
-        words = [line.strip() for line in open('words.txt') if 6 < len(line) <= 9]
+        words = [line.strip() for line in open(base_path('words.txt')) if 6 < len(line.strip()) <= 9]
     else:
-        words = [line.strip() for line in open('words.txt') if 9 < len(line)]
+        words = [line.strip() for line in open(base_path('words.txt')) if 9 < len(line.strip())]
     return random.choice(words).upper()
 
 class Game(db.Model):
@@ -43,22 +45,32 @@ class Game(db.Model):
     tried = db.Column(db.String(50), default='')
     player = db.Column(db.String(50))
 
+    # Initializing the game
+
     def __init__(self, player, difficulty):
         self.player = player.capitalize()
         self.difficulty = difficulty
         self.word = random_word(difficulty.upper())
 
+    # Returns the String of letters the user got wrong
+
     @property
     def errors(self):
         return ''.join(set(self.tried) - set(self.word))
+
+    # Returns the current state of the game
 
     @property
     def current(self):
         return ''.join([c if c in self.tried else '_' for c in self.word])
 
+    # Returns the points earned by the player
+
     @property
     def points(self):
-        return 100 + 2*len(set(self.word)) + len(self.word) - 10*len(self.errors)
+        return 100 + 2*len(set(self.word)) + len(self.word) - 10*len(self.real_errors)
+
+    # Returns the number of errors the player made after the user got the first letter right
 
     @property
     def real_errors(self):
@@ -70,6 +82,8 @@ class Game(db.Model):
         return ''.join(set(str1) - set(self.word))
 
     # Play
+    # Checks if the input is a letter and if it is in the word
+    # Else checks if the input is of the same length as the current word and if it is the same
 
     def try_letter(self, letter):
         if not self.finished:
@@ -102,13 +116,22 @@ class Game(db.Model):
 
 
 # Controller
+# Sorts the database according to points and divides them according to difficulty 
 
 @app.route('/')
 def home():
-    games = sorted(
-        [game for game in Game.query.all() if game.won],
-        key=lambda game: -game.points)[:10]
-    return flask.render_template('home.html', games=games)
+    games_easy = sorted(
+        [game for game in Game.query.all() if game.won and 4 <= len(game.current) <= 6],
+        key=lambda game: -game.points)[:5]
+    games_medium = sorted(
+        [game for game in Game.query.all() if game.won and 6 < len(game.current) <= 9],
+        key=lambda game: -game.points)[:5]
+    games_hard = sorted(
+        [game for game in Game.query.all() if game.won and len(game.current) > 9],
+        key=lambda game: -game.points)[:5]
+    return flask.render_template('home.html', games_easy=games_easy, games_medium = games_medium, games_hard = games_hard)
+
+# Starts a new game with player name and difficulty
 
 @app.route('/play')
 def new_game():
@@ -120,14 +143,18 @@ def new_game():
     time.sleep(2)
     return flask.redirect(flask.url_for('play', game_id=game.pk))
 
+# 
+
 @app.route('/play/<game_id>', methods=['GET', 'POST'])
 def play(game_id):
     game = Game.query.get_or_404(game_id)
 
     if flask.request.method == 'POST':
         letter = flask.request.form['letter'].upper()
-        if letter.isalpha() and (len(letter) == 1 or len(letter) == len(game.word)):
-            game.try_letter(letter)
+
+        # Checking if the letter is an alphabet and length of the input is 1 or the length of the word
+        if letter.isalpha() and (len(letter) == 1 or len(letter.strip()) == len(game.word)):
+            game.try_letter(letter.strip())
 
     if flask.request.is_xhr:
         return flask.jsonify(current=game.current,
@@ -140,4 +167,12 @@ def play(game_id):
 # Main
 
 if __name__ == '__main__':
+
+    # Opening the web address of the game
+    webbrowser.open_new_tab("http://127.0.0.1:5000/")
+
+    # Changing the directory
+    os.chdir(base_path(''))
+
+    # Runnning the game
     app.run()
